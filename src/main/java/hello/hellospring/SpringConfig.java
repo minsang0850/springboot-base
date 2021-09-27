d0fc6b26e0d80ecce0d50747cca83b3771f4b24d
park <minsang0850@naver.com>
2021-09-27 16:11:00 +0900

    make spring bean and implement web mvc

---
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -0,0 +1,13 @@
+package hello.hellospring;
+import org.springframework.context.annotation.Configuration;
+@Configuration
+public class SpringConfig {
+//    @Bean
+//    public MemberService memberService() {
+//        return new MemberService(memberRepository());
+//    }
+//    @Bean
+//    public MemberRepository memberRepository() {
+//        return new MemoryMemberRepository();
+//    }
+}
